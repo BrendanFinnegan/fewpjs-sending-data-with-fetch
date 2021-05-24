8860939bccfb4a928d84243d8cf5bd1bb728b289
BrendanFinnegan <bf289907@gmail.com>
2021-05-24 18:16:50 -0400

    commit tree b83aac4081562d86024e3dea81857b1bd8cc0335

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Add your code here
+
+const configurationObject =  {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    },
+body: JSON.stringify({
+    dogName: "beep",
+    dogBreed: "bop"
+})
+}
+fetch(`http://localhost:3000/dogs`, configurationObject).then(response=> response.json()).then(object => console.log(object)).catch(error => {alert("Bad Things");
+ console.log(error.message)})
+
+ 
+
+function submitData(a,b) {
+    let obj = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: a,
+            email: b
+        })
+      }; 
+     
+     return fetch(`http://localhost:3000/users`, obj).then(response=> response.json()).then(object => document.body.innerHTML = (object["id"])).catch(error => document.body.innerHTML = error.message)
+    }
+ submitData(`bob`, `bill.com`)
